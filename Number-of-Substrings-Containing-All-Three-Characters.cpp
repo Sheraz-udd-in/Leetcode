7cf92b302c1622ea16bd00c49386e609085b7995
Sheraz uddin mirza <sherazuddin76@gmail.com>
2025-07-06 20:28:51 +0530

    Two Pointer and Sliding Window

--- Number-of-Substrings-Containing-All-Three-Characters.cpp
+++ Number-of-Substrings-Containing-All-Three-Characters.cpp
@@ -3,13 +3,13 @@ using namespace std;
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int abc[3] = {-1, -1, -1};
+        int xyz[3] = {-1, -1, -1};
         int count = 0, right = 0;
         while (right < s.length()) {
-            abc[s[right] - 'a'] = right;
+            xyz[s[right] - 'a'] = right;
             int minIndex = INT_MAX;
             for (int i = 0; i < 3; i++) {
-                minIndex = min(minIndex, abc[i]);
+                minIndex = min(minIndex, xyz[i]);
             }
             count += (minIndex + 1);
             right++;
