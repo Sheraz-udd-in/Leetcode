30d2a65125d563c622aea686cab4dec765b71814
Sheraz uddin mirza <sherazuddin76@gmail.com>
2025-07-07 22:35:20 +0530

    Two Pointer and Sliding Window

--- Max-Consecutive-Ones-III.cpp
+++ Max-Consecutive-Ones-III.cpp
@@ -3,19 +3,19 @@ using namespace std;
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        int left = 0, maxLength = 0, zeroCount = 0;
+        int l = 0, maxLength = 0, zeroCount = 0;
 
-        for (int right = 0; right < nums.size(); ++right) {
-            if (nums[right] == 0) {
+        for (int r = 0; r < nums.size(); ++r) {
+            if (nums[r] == 0) {
                 zeroCount++;
             }
             while (zeroCount > k) {
-                if (nums[left] == 0) {
+                if (nums[l] == 0) {
                     zeroCount--;
                 }
-                left++;
+                l++;
             }
-            maxLength = max(maxLength, right - left + 1);
+            maxLength = max(maxLength, r - l + 1);
         }
 
         return maxLength;
