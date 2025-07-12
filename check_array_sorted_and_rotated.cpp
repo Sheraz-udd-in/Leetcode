37fe185bac20bbe57b0614ddc5faa65a9fdd3bfd
Sheraz uddin mirza <sherazuddin76@gmail.com>
2025-07-12 21:22:13 +0530

    Arrays

--- check_array_sorted_and_rotated.cpp
+++ check_array_sorted_and_rotated.cpp
@@ -4,15 +4,15 @@ class Solution {
 public:
     bool check(vector<int>& nums) {
         int n = nums.size();
-        int cnt =0;
+        int count =0;
         for(int i=1;i<n;i++){
             if(nums[i-1] > nums[i]){
-                cnt++;
+                count++;
             }
         }
         if(nums[n-1] > nums[0]){
-            cnt++;
+            count++;
         }
-        return cnt <=1;
+        return count <=1;
     }
 };
