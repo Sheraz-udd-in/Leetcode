7ff40218b6dda540cc8be9f46e6902ed0fa575ba
Sheraz uddin mirza <sherazuddin76@gmail.com>
2025-07-25 21:52:37 +0530

    Arrays

--- sort_colors.cpp
+++ sort_colors.cpp
@@ -3,11 +3,11 @@ using namespace std;
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int low = 0,mid=0,high=nums.size()-1;
+        int l = 0,mid=0,high=nums.size()-1;
         while(mid<=high){
             if(nums[mid]==0){
-                swap(nums[low],nums[mid]);
-                low++;
+                swap(nums[l],nums[mid]);
+                l++;
                 mid++;
             }
             else if(nums[mid]== 2){
