3840ec4484ce5e44cf0979bb503ad5371a0224b6
Sheraz uddin mirza <sherazuddin76@gmail.com>
2025-07-27 20:27:36 +0530

    Arrays

--- Two_sum.cpp
+++ Two_sum.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int>& nums, int tar) {
         int n = nums.size();
         unordered_map<int,int> mp;
         for(int i=0;i<n;i++){
-            int rem = target - nums[i];
+            int rem = tar - nums[i];
             if(mp.find(rem) != mp.end()){
                 return {i,mp[rem]};
             }
