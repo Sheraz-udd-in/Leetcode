be60a08a4aa6ab7694e91b1a218da79aa5098c40
Sheraz uddin mirza <sherazuddin76@gmail.com>
2025-07-28 23:51:33 +0530

    Two Pointer and Sliding Window

--- count_number_of_Nice_Subarrays.cpp
+++ count_number_of_Nice_Subarrays.cpp
@@ -4,15 +4,15 @@ class Solution {
 public:
     int func(vector<int> & nums,int k){
         int n = nums.size();
-        int l=0,r=0,sum=0,cnt=0;
-        while(r<n){
-            sum += nums[r];
+        int left=0,right=0,sum=0,cnt=0;
+        while(right<n){
+            sum += nums[right];
             while(sum >k){
-                sum -= nums[l];
-                l++;
+                sum -= nums[left];
+                left++;
             }
-            cnt = cnt + (r-l+1);
-            r++;
+            cnt = cnt + (right-left+1);
+            right++;
         }
         return cnt;
     }
